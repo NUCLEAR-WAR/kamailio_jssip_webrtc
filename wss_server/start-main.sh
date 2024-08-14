9c242222d6e6c413e6119735b69da4e468285a44
NUCLEAR-WAR <abdo838@hotmail.com>
2024-08-14 07:09:37 +0200

    Initial commit

--- wss_server/start-main.sh
+++ wss_server/start-main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 /mnt/wss_server/start-wssserver.sh && \
-/usr/bin/supervisord -n && \
-mkdir -p /var/run/kamailio_wss && \
+/mnt/wss_server/start-nginx.sh && \
 rm -f /kamailio_wss.pid && \
 kamailio -f /etc/kamailio/kamailio.cfg -P /kamailio_wss.pid -DD -E -e
